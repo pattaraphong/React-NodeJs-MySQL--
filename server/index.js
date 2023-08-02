ff834cf8a78764a32a025c72e54dbd6b7c3080ed
pattaraphong <illsnick037@gmail.com>
2023-08-03 02:40:34 +0700

    first commit

--- server/index.js
+++ server/index.js
@@ -56,8 +56,55 @@ app.delete('/delete/:id', (req, res) =>{
 })
 
 
+
+app.get('/edit/:id', (req, res) =>{
+    const sql = "SELECT * FROM product WHERE ID = ?";
+    const id = req.params.id;
+    db.query(sql,[id], (err, result) => {
+        if(err) return res.json({Error: err});
+        return res.json(result);
+    }) 
+})
+
+
+//PUT ใช้อันไหนก็ได้ //
+
+// ถ้าใช้อันนี้อย่าลืมใส่ .updated ตรง if(res.data.updated) ใน funtion hanldeSubmit
+
+// app.put('/update/:id', (req, res) => {
+//     const sql = "UPDATE product SET `name` = ?, `codename` = ?, `price` = ?, `detail` = ?, `timeproduct` = ?, `dateproduct` = ? WHERE ID = ?";
+//     const id = req.params.id;
+//     db.query(sql, [ req.body.name, req.body.codename, req.body.price, req.body.detail, req.body.timeproduct, req.body.dateproduct, id],
+//         (err, result) => {
+//             if(err) return res.json("Error");
+//             return res.json({updated: true})
+//         })
+// })
+
+app.put('/update/:id', (req, res) =>{
+    const id = req.params.id;
+    
+    const name = req.body.name;
+    const codename = req.body.codename;
+    const price = req.body.price;
+    const detail = req.body.detail;
+    const timeproduct = req.body.timeproduct;
+    const dateproduct = req.body.dateproduct;
+
+    db.query("UPDATE product SET name = ?, codename = ?, price = ?, detail = ?, timeproduct = ?, dateproduct = ? WHERE id = ?",
+    [name, codename, price, detail, timeproduct, dateproduct, id],(err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+        }
+    })
+})
+
+
 app.listen('3001', () =>{
     console.log('server is running port 3001')
+    
 })
 
   
